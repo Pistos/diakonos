cae1c54652bf13647e0bb4e824de1db05043b19e
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2006-02-02 22:06:04 +0000

    Removed several "self" references out of class String. Reorganized initialization code, moving curses drawing as late as possible.

--- test/string-test.rb
+++ test/string-test.rb
@@ -4,26 +4,44 @@ require 'test/unit'
 require 'diakonos'
 
 class TC_String < Test::Unit::TestCase
+    def setup
+        $diakonos = Diakonos.new
+    end
+    
     def test_subHome
         s = "/test"
         assert_equal(
-            "/test", s.subHome,
+            "/test",
+            s.subHome,
             "Original string: '#{s}'"
         )
         s = "~/test"
         assert_equal(
-            "#{ENV[ 'HOME' ]}/test", s.subHome,
+            "#{ENV[ 'HOME' ]}/test",
+            s.subHome,
             "Original string: '#{s}'"
         )
         s = "/this/is/~/test"
         assert_equal(
-            "/this/is/#{ENV[ 'HOME' ]}/test", s.subHome,
+            "/this/is/#{ENV[ 'HOME' ]}/test",
+            s.subHome,
             "Original string: '#{s}'"
         )
         s = "~"
         assert_equal(
-            ENV[ 'HOME' ], s.subHome,
+            ENV[ 'HOME' ],
+            s.subHome,
             "Original string: '#{s}'"
         )
+    end
+    
+    def test_indentation_level
+        #indentation_level( indent_size, indent_roundup, indent_ignore_charset = nil )
+        s = "x"
+        assert_equal( 0, s.indentation_level( 4, true ) )
+        assert_equal( 0, s.indentation_level( 4, false ) )
+        s = "  x"
+        assert_equal( 1, s.indentation_level( 4, true ) )
+        assert_equal( 0, s.indentation_level( 4, false ) )
     end
 end
