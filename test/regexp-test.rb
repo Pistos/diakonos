ae6d81bd80a184921031e9edcc2366331ca56518
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2006-02-02 21:07:15 +0000

    Renamed usesBOS to uses_bos. Added another assertion to regexp-test.

--- test/regexp-test.rb
+++ test/regexp-test.rb
@@ -4,16 +4,23 @@ require 'test/unit'
 require 'diakonos'
 
 class TC_Regexp < Test::Unit::TestCase
-    def test_usesBOS
+    def test_uses_bos
         r = /^test/
         assert_equal(
-            true, r.usesBOS,
-            "/#{r.source}/.usesBOS should be true"
+            true, r.uses_bos,
+            "/#{r.source}/.uses_bos should be true"
         )
+        
         r = /test/
         assert_equal(
-            false, r.usesBOS,
-            "/#{r.source}/.usesBOS should be false"
+            false, r.uses_bos,
+            "/#{r.source}/.uses_bos should be false"
+        )
+        
+        r = /t^est/
+        assert_equal(
+            false, r.uses_bos,
+            "/#{r.source}/.uses_bos should be false"
         )
     end
 end
