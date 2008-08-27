5f791898ca1b20a91d0ca124628c2b80a7f30554
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-08-27 08:00:43 -0400

    Changed indentation of test file.

--- test/buffer-test.rb
+++ test/buffer-test.rb
@@ -4,34 +4,34 @@ require 'test/unit'
 require 'diakonos'
 
 class TC_Buffer < Test::Unit::TestCase
-    SAMPLE_FILE = File.dirname( File.expand_path( __FILE__ ) ) + '/sample-file.rb'
-    
-    def setup
-        @d = Diakonos::Diakonos.new [ '-e', 'quit' ]
-        @d.start
-    end
-    
-    def teardown
-        system "reset"
-    end
-    
-    def test_selected_text
-        @d.openFile( SAMPLE_FILE )
-        b = Diakonos::Buffer.new( @d, SAMPLE_FILE, SAMPLE_FILE )
-        @d.anchorSelection
-        @d.cursorDown
-        @d.cursorDown
-        @d.cursorDown
-        @d.copySelection
-        assert_equal(
-            [
-                "#!/usr/bin/env ruby",
-                "",
-                "# This is only a sample file used in the tests.",
-                ""
-            ],
-            @d.clipboard.clip
-        )
-    end
-    
+  SAMPLE_FILE = File.dirname( File.expand_path( __FILE__ ) ) + '/sample-file.rb'
+  
+  def setup
+    @d = Diakonos::Diakonos.new [ '-e', 'quit' ]
+    @d.start
+  end
+  
+  def teardown
+    system "reset"
+  end
+  
+  def test_selected_text
+    @d.openFile( SAMPLE_FILE )
+    b = Diakonos::Buffer.new( @d, SAMPLE_FILE, SAMPLE_FILE )
+    @d.anchorSelection
+    @d.cursorDown
+    @d.cursorDown
+    @d.cursorDown
+    @d.copySelection
+    assert_equal(
+      [
+        "#!/usr/bin/env ruby",
+        "",
+        "# This is only a sample file used in the tests.",
+        ""
+      ],
+      @d.clipboard.clip
+    )
+  end
+  
 end
