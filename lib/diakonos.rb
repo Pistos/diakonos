25c8845e50d9bec9b4b856613b052c45be79bb50
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-10-10 15:13:46 -0400

    Whitespace adjustment and minor refactoring of Diakonos#processKeystroke.

--- lib/diakonos.rb
+++ lib/diakonos.rb
@@ -843,85 +843,83 @@ class Diakonos
     # context is an array of characters (bytes) which are keystrokes previously
     # typed (in a chain of keystrokes)
     def processKeystroke( context = [] )
-        c = @win_main.getch
+      c = @win_main.getch
         
-        if @capturing_keychain
-            if c == ENTER
-                @capturing_keychain = false
-                @current_buffer.deleteSelection
-                str = context.to_keychain_s.strip
-                @current_buffer.insertString str 
-                cursorRight( Buffer::STILL_TYPING, str.length )
-            else
-                keychain_pressed = context.concat [ c ]
-                
-                function_and_args = @keychains.getLeaf( keychain_pressed )
-                
-                if function_and_args != nil
-                    function, args = function_and_args
-                end
-                
-                partial_keychain = @keychains.getNode( keychain_pressed )
-                if partial_keychain != nil
-                    setILine( "Part of existing keychain: " + keychain_pressed.to_keychain_s + "..." )
-                else
-                    setILine keychain_pressed.to_keychain_s + "..."
-                end
-                processKeystroke( keychain_pressed )
-            end
+      if @capturing_keychain
+        if c == ENTER
+          @capturing_keychain = false
+          @current_buffer.deleteSelection
+          str = context.to_keychain_s.strip
+          @current_buffer.insertString str 
+          cursorRight( Buffer::STILL_TYPING, str.length )
         else
-        
-            if context.empty?
-                if c > 31 and c < 255 and c != BACKSPACE
-                    if @macro_history != nil
-                        @macro_history.push "typeCharacter #{c}"
-                    end
-                    if not @there_was_non_movement
-                        @there_was_non_movement = true
-                    end
-                    typeCharacter c
-                    return
-                end
-            end
-            keychain_pressed = context.concat [ c ]
-            
-            function_and_args = @keychains.getLeaf( keychain_pressed )
-            
-            if function_and_args != nil
-                function, args = function_and_args
-                setILine if not @settings[ "context.combined" ]
-                
-                if args != nil
-                    to_eval = "#{function}( #{args} )"
-                else
-                    to_eval = function
-                end
-                
-                if @macro_history != nil
-                    @macro_history.push to_eval
-                end
-                
-                begin
-                    eval to_eval, nil, "eval"
-                    @last_commands << to_eval unless to_eval == "repeatLast"
-                    if not @there_was_non_movement
-                        @there_was_non_movement = ( not to_eval.movement? )
-                    end
-                rescue Exception => e
-                    debugLog e.message
-                    debugLog e.backtrace.join( "\n\t" )
-                    showException e
-                end
-            else
-                partial_keychain = @keychains.getNode( keychain_pressed )
-                if partial_keychain != nil
-                    setILine( keychain_pressed.to_keychain_s + "..." )
-                    processKeystroke( keychain_pressed )
-                else
-                    setILine "Nothing assigned to #{keychain_pressed.to_keychain_s}"
-                end
-            end
+          keychain_pressed = context.concat [ c ]
+          
+          function_and_args = @keychains.getLeaf( keychain_pressed )
+          
+          if function_and_args
+            function, args = function_and_args
+          end
+          
+          partial_keychain = @keychains.getNode( keychain_pressed )
+          if partial_keychain
+            setILine( "Part of existing keychain: " + keychain_pressed.to_keychain_s + "..." )
+          else
+            setILine keychain_pressed.to_keychain_s + "..."
+          end
+          processKeystroke( keychain_pressed )
         end
+      else
+        
+        if context.empty?
+          if c > 31 and c < 255 and c != BACKSPACE
+            if @macro_history
+              @macro_history.push "typeCharacter #{c}"
+            end
+            @there_was_non_movement = true
+            typeCharacter c
+            return
+          end
+        end
+        keychain_pressed = context.concat [ c ]
+            
+        function_and_args = @keychains.getLeaf( keychain_pressed )
+        
+        if function_and_args
+          function, args = function_and_args
+          setILine if not @settings[ "context.combined" ]
+          
+          if args
+            to_eval = "#{function}( #{args} )"
+          else
+            to_eval = function
+          end
+          
+          if @macro_history
+            @macro_history.push to_eval
+          end
+          
+          begin
+            eval to_eval, nil, "eval"
+            @last_commands << to_eval unless to_eval == "repeatLast"
+            if not @there_was_non_movement
+              @there_was_non_movement = ( not to_eval.movement? )
+            end
+          rescue Exception => e
+            debugLog e.message
+            debugLog e.backtrace.join( "\n\t" )
+            showException e
+          end
+        else
+          partial_keychain = @keychains.getNode( keychain_pressed )
+          if partial_keychain
+            setILine( keychain_pressed.to_keychain_s + "..." )
+            processKeystroke( keychain_pressed )
+          else
+            setILine "Nothing assigned to #{keychain_pressed.to_keychain_s}"
+          end
+        end
+      end
     end
     protected :processKeystroke
 
