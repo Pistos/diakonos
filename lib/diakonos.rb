9102dc1903901d2aeb76a0a2293930047824f0b8
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-12-07 12:14:00 -0500

    Functional fuzzy file finding on openFileAsk.

--- lib/diakonos.rb
+++ lib/diakonos.rb
@@ -53,7 +53,7 @@ require 'diakonos/window'
 require 'diakonos/clipboard'
 require 'diakonos/readline'
 
-require 'diakonos/vendor/fuzzy_file_finder'
+require 'vendor/fuzzy_file_finder'
 
 #$profiling = true
 
