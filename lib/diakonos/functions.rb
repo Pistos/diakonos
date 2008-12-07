1e523d8b441f63ba593457f85820eb01de56e2d3
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-12-07 14:51:47 -0500

    Ruby's puts takes an Array and separates with \n for you.  Thanks, apeiros!  :)

--- lib/diakonos/functions.rb
+++ lib/diakonos/functions.rb
@@ -774,7 +774,7 @@ module Diakonos
           finder = FuzzyFileFinder.new
           matches = finder.find( input ).sort_by { |m| [ -m[:score], m[:path] ] }
           with_list_file do |list|
-            list.puts matches.map { |m| m[ :path ] }.join( "\n" )
+            list.puts matches.map { |m| m[ :path ] }
           end
           openListBuffer
         }
