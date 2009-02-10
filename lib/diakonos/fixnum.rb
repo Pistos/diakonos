d36ddad7badbb1b454346ca713622251c5125687
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-02-10 08:46:30 -0500

    Added Fixnum#ord for 1.8 compatibility.

--- lib/diakonos/fixnum.rb
+++ lib/diakonos/fixnum.rb
@@ -1,13 +1,17 @@
 require 'diakonos/keycode'
 
 class Fixnum
-    include Diakonos::KeyCode
+  include Diakonos::KeyCode
 
-    def fit( min, max )
-        return self if max < min
-        return min if self < min
-        return max if self > max
-        return self
-    end
+  def fit( min, max )
+    return self if max < min
+    return min if self < min
+    return max if self > max
+    return self
+  end
+
+  def ord
+    self
+  end
 end
 
