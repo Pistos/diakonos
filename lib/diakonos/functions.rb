e780fbf771170271d3b0c3d50bfa314776cee216
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-03-04 12:52:40 -0500

    Fixed delete_to* functions; now add to clipboard.

--- lib/diakonos/functions.rb
+++ lib/diakonos/functions.rb
@@ -314,7 +314,7 @@ module Diakonos
       if char
         removed_text = @current_buffer.delete_to char
         if removed_text
-          @clipboard.addClip removed_text
+          @clipboard.addClip( [ removed_text ] )
         else
           setILine "'#{char}' not found."
         end
@@ -330,7 +330,7 @@ module Diakonos
       if char
         removed_text = @current_buffer.delete_to_and_from char
         if removed_text
-          @clipboard.addClip removed_text
+          @clipboard.addClip( [ removed_text ] )
         else
           setILine "'#{char}' not found."
         end
