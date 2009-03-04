5eb2dd6ee850e8a951184a6a572e13e78c51dbc7
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-03-04 11:32:14 -0500

    Moved find_ from diakonos.rb to functions.rb.

--- lib/diakonos.rb
+++ lib/diakonos.rb
@@ -559,53 +559,6 @@ module Diakonos
       true
     end
 
-    # Worker method for find function.
-    def find_( direction, case_sensitive, regexp_source, replacement, starting_row, starting_col, quiet )
-      return if( regexp_source.nil? or regexp_source.empty? )
-
-      rs_array = regexp_source.newlineSplit
-      regexps = Array.new
-      exception_thrown = nil
-
-      rs_array.each do |source|
-        begin
-          warning_verbosity = $VERBOSE
-          $VERBOSE = nil
-          regexps << Regexp.new(
-            source,
-            case_sensitive ? nil : Regexp::IGNORECASE
-          )
-          $VERBOSE = warning_verbosity
-        rescue RegexpError => e
-          if not exception_thrown
-            exception_thrown = e
-            source = Regexp.escape( source )
-            retry
-          else
-            raise e
-          end
-        end
-      end
-
-      if replacement == ASK_REPLACEMENT
-        replacement = getUserInput( "Replace with: ", @rlh_search )
-      end
-
-      if exception_thrown and not quiet
-        setILine( "Searching literally; #{exception_thrown.message}" )
-      end
-
-      @current_buffer.find(
-        regexps,
-        :direction    => direction,
-        :replacement  => replacement,
-        :starting_row => starting_row,
-        :starting_col => starting_col,
-        :quiet        => quiet
-      )
-      @last_search_regexps = regexps
-    end
-
     def settings
       @settings.merge @session[ 'settings' ]
     end
--- lib/diakonos/functions.rb
+++ lib/diakonos/functions.rb
@@ -375,6 +375,53 @@ module Diakonos
         end
     end
 
+    # Worker method for find function.
+    def find_( direction, case_sensitive, regexp_source, replacement, starting_row, starting_col, quiet )
+      return if( regexp_source.nil? or regexp_source.empty? )
+
+      rs_array = regexp_source.newlineSplit
+      regexps = Array.new
+      exception_thrown = nil
+
+      rs_array.each do |source|
+        begin
+          warning_verbosity = $VERBOSE
+          $VERBOSE = nil
+          regexps << Regexp.new(
+            source,
+            case_sensitive ? nil : Regexp::IGNORECASE
+          )
+          $VERBOSE = warning_verbosity
+        rescue RegexpError => e
+          if not exception_thrown
+            exception_thrown = e
+            source = Regexp.escape( source )
+            retry
+          else
+            raise e
+          end
+        end
+      end
+
+      if replacement == ASK_REPLACEMENT
+        replacement = getUserInput( "Replace with: ", @rlh_search )
+      end
+
+      if exception_thrown and not quiet
+        setILine( "Searching literally; #{exception_thrown.message}" )
+      end
+
+      @current_buffer.find(
+        regexps,
+        :direction    => direction,
+        :replacement  => replacement,
+        :starting_row => starting_row,
+        :starting_col => starting_col,
+        :quiet        => quiet
+      )
+      @last_search_regexps = regexps
+    end
+
     def find( dir_str = "down", case_sensitive = CASE_INSENSITIVE, regexp_source_ = nil, replacement = nil )
       direction = dir_str.toDirection
       if regexp_source_.nil?
