282966e4d79369146d9ac7582408312ef21ba3cc
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-03-10 20:37:45 -0400

    Changed indentation a bit. Added tests for Buffer#indentation_level; some are failing!

--- test/sample-file.rb
+++ test/sample-file.rb
@@ -3,15 +3,15 @@
 # This is only a sample file used in the tests.
 
 class Sample
-    def initialize
-        @x = 1
-        @y = 2
-    end
-    
-    def printout
-        puts x
-        puts y
-    end
+  def initialize
+    @x = 1
+    @y = 2
+  end
+
+  def printout
+    puts x
+    puts y
+  end
 end
 
 s = Sample.new
