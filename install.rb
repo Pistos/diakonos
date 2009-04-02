d27c8414b4877aac3283468b462d0c5f5b79718b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-04-02 16:41:39 -0400

    Refactored installer a lot.  Thank you to manveru for many suggestions and changes.

--- install.rb
+++ install.rb
@@ -8,28 +8,39 @@ require 'rbconfig'
 module Diakonos
   class Installer
 
+    def strip_prefix( str )
+      str.sub( %r{^#{Regexp.escape( @prefix )}/*}, '' )
+    end
+
     def initialize( argv = ARGV.dup )
-      want_help = false
-      @prefix = '/usr'
-      @lib_dir = $LOAD_PATH.grep( /site_ruby/ ).first
-      @conf_dir = nil
-      @bin_dir = nil
-      @doc_dir = nil
-      @verbose = false
-      @pretend = false
-      @bin_suffix = 'bin'
-      @conf_suffix = '../etc'
-      @doc_suffix = 'share/doc'
+      rconfig = RbConfig::CONFIG
+
+      bang_ruby = File.join( rconfig[ 'bindir' ], rconfig[ 'ruby_install_name' ] )
+      @shebang = "#!#{bang_ruby}"
+
+      want_help    = false
+      @verbose     = false
+      @pretend     = false
+
+      @dest_dir    = '/'
+      @lib_dir     = nil
+      @conf_dir    = nil
+      @bin_dir     = nil
+      @doc_dir     = nil
+
+      @prefix      = rconfig[ 'prefix' ]
+      @bin_suffix  = strip_prefix( rconfig[ 'bindir' ] )
+      @lib_suffix  = strip_prefix( rconfig[ 'sitelibdir' ] )
+      @conf_dir    = rconfig[ 'sysconfdir' ]
+      @doc_suffix  = strip_prefix( rconfig[ 'docdir' ].sub( /\$\(PACKAGE\)/, "diakonos-#{Diakonos::VERSION}" ) )
 
       while argv.any?
         arg = argv.shift
         case arg
-        when '-h', '--help'
-          want_help = true
+        when '--dest-dir'
+          @dest_dir = argv.shift
         when '--prefix'
           @prefix = argv.shift
-        when '-p', '--pretend', '--dry-run'
-          @pretend = true
         when '--bin-dir'
           @bin_dir = argv.shift
         when '--conf-dir'
@@ -40,14 +51,16 @@ module Diakonos
           @lib_dir = argv.shift
         when '-v', '--verbose'
           @verbose = true
+        when '-p', '--pretend', '--dry-run'
+          @pretend = true
+        when '-h', '--help'
+          want_help = true
         end
       end
 
-      @bin_dir ||= "#{@prefix}/#{@bin_suffix}"
-      @doc_dir ||= "#{@prefix}/#{@doc_suffix}"
-      @conf_dir ||= "#{@prefix}/#{@conf_suffix}"
-
-      @versioned_package = "diakonos-#{Diakonos::VERSION}"
+      @bin_dir  ||= File.join( @prefix, @bin_suffix )
+      @lib_dir  ||= File.join( @prefix, @lib_suffix )
+      @doc_dir  ||= File.join( @prefix, @doc_suffix )
 
       if want_help
         print_usage_and_exit
@@ -78,29 +91,31 @@ module Diakonos
       puts "    -v / --verbose       print each step"
       puts "    -p / --pretend       don't actually do anything"
       puts "    --prefix <path>      set installation prefix (default #{@prefix})"
+      puts "    --dest-dir <path>    set installation sandbox dir (default #{@dest_dir})"
       puts "    --bin-dir <path>     set executable installation dir (default <prefix>/#{@bin_suffix})"
       puts "    --doc-dir <path>     set documentation installation dir (default <prefix>/#{@doc_suffix})"
-      puts "    --conf-dir <path>    set configuration installation dir (default <prefix>/#{@conf_suffix})"
-      puts "    --lib-dir <path>     set library installation dir (default on this system: #{@lib_dir})"
+      puts "    --conf-dir <path>    set configuration installation dir (default #{@conf_dir})"
+      puts "    --lib-dir <path>     set library installation dir (default <prefix>/#{@lib_suffix})"
       exit 2
     end
 
     def write_installation_settings
-      installation_file = "#{@lib_dir}/diakonos/installation.rb"
+      installation_file = File.join( @lib_dir, 'diakonos', 'installation.rb' )
       if @verbose
         puts "Writing installation settings to #{installation_file}"
       end
       return  if @pretend
+
       File.open( installation_file, 'w' ) do |f|
         f.puts %|
 module Diakonos
   INSTALL_SETTINGS = {
-    :prefix   => '#{@prefix}',
-    :bin_dir  => '#{@bin_dir}',
-    :doc_dir  => '#{@doc_dir}',
-    :help_dir => '#{@help_dir}',
-    :conf_dir => '#{@conf_dir}',
-    :lib_dir  => '#{@lib_dir}',
+    :prefix   => #{@prefix.dump},
+    :bin_dir  => #{@bin_dir.dump},
+    :doc_dir  => #{@doc_dir.dump},
+    :help_dir => #{@help_dir.dump},
+    :conf_dir => #{@conf_dir.dump},
+    :lib_dir  => #{@lib_dir.dump},
     :installed => {
       :files => #{@installed_files.pretty_inspect.strip},
       :dirs => #{@installed_dirs.pretty_inspect.strip},
@@ -112,20 +127,15 @@ end
     end
 
     def cp_( source, dest )
-      cp source, dest
-      case source
-        when Array
-          source.each do |f|
-            @installed_files << File.expand_path( "#{dest}/#{File.basename(f)}" )
-          end
-        else
-          @installed_files << File.expand_path( "#{dest}/#{File.basename(source)}" )
+      cp source, File.join( @dest_dir, dest )
+      Array( source ).each do |file|
+        @installed_files << File.expand_path( File.join( dest, File.basename( file ) ) )
       end
     end
 
     def mkdir_( dir )
       begin
-        mkdir_p dir, :mode => 0755
+        mkdir_p File.join( @dest_dir, dir ), :mode => 0755
       rescue Errno::EEXIST
         # Don't panic if the directory already exists
       end
@@ -133,25 +143,28 @@ end
     end
 
     def install_( source, dest )
-      # Rewrite bang line
-      ruby = File.join( RbConfig::CONFIG.values_at( 'bindir', 'ruby_install_name' ) )
-      tmp = "diakonos-#{rand(9999999)}.tmp"
-      command = %{/bin/bash -c 'cat <( echo "#!#{ruby}" ) <( tail -n +2 #{source} ) > #{tmp}'}
+      dest_file = File.join( dest, File.basename( source ) )
+      dest_dir_file = File.join( @dest_dir, dest_file )
+
+      install source, dest_dir_file, :mode => 0755
+
+      # Rewrite shebang line
+      command = "sed -i_ '1s|.*|#{@shebang}|' #{dest_dir_file}"
+
       if @verbose
         puts command
       end
       if ! @pretend
         system command
+        rm "#{dest_dir_file}_"
       end
 
-      installed = File.expand_path( "#{dest}/#{File.basename(source)}" )
-      install tmp, installed, :mode => 0755
-      rm_f tmp
-      @installed_files << installed
+      @installed_files << dest_file
     end
 
     def run
       # Libraries
+      mkdir_ @lib_dir
       cp_ 'lib/diakonos.rb', @lib_dir
 
       dir = "#{@lib_dir}/diakonos"
@@ -164,16 +177,17 @@ end
       cp_ Dir[ 'lib/diakonos/vendor/*.rb' ], dir
 
       # Configuration
+      mkdir_ @conf_dir
       cp_ %w( diakonos.conf diakonos-256-colour.conf ), @conf_dir
 
       # Executables
+      mkdir_ @bin_dir
       install_ 'bin/diakonos', @bin_dir
 
       # Documentation
-      dir = "#{@doc_dir}/#{@versioned_package}"
-      @help_dir = "#{dir}/help"
+      @help_dir = "#{@doc_dir}/help"
       mkdir_ @help_dir
-      cp_ %w( README CHANGELOG LICENCE ), dir
+      cp_ %w( README CHANGELOG LICENCE ), @doc_dir
       cp_ Dir[ 'help/*' ], @help_dir
 
       write_installation_settings
@@ -186,7 +200,11 @@ installer.run
 
 puts %{
 Diakonos #{Diakonos::VERSION} (#{Diakonos::LAST_MODIFIED}) installed.
-Diakonos can be uninstalled by running:
+
+If Diakonos has been installed with a Linux distro's package manager,
+uninstall using that package manager.
+If Diakonos has been installed independently of a package manager,
+uninstall by running:
 
   diakonos --uninstall
 
