dcaea9758a5634da28092c034fdee3ef0700fa92
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-04-18 00:43:09 -0400

    Filled out the specs of the rest of the main cursor functions.

--- spec/functions/cursor.rb
+++ spec/functions/cursor.rb
@@ -1,5 +1,10 @@
 require 'spec/preparation'
 
+def cursor_should_be_at( row, col )
+  @b.current_row.should.equal row
+  @b.current_column.should.equal col
+end
+
 describe 'Diakonos' do
 
   before do
@@ -9,8 +14,26 @@ describe 'Diakonos' do
 
   it 'allows basic cursor movements' do
     original_lines = @b.to_a
-    @b.current_row.should.equal 0
-    @b.current_column.should.equal 0
+    cursor_should_be_at 0,0
+
+    @d.cursor_down
+    cursor_should_be_at 1,0
+    @d.cursor_up
+    cursor_should_be_at 0,0
+    @d.cursor_right
+    cursor_should_be_at 0,1
+    @d.cursor_left
+    cursor_should_be_at 0,0
+
+    @d.cursor_eol
+    cursor_should_be_at 0,19
+    @d.cursor_bol
+    cursor_should_be_at 0,0
+
+    @d.cursor_eof
+    cursor_should_be_at 20,0
+    @d.cursor_bof
+    cursor_should_be_at 0,0
   end
 
 end
