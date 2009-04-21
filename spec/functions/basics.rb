5788ce134116dff7a1bcd5dc2f6233280ada313f
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-04-21 09:11:23 -0400

    Added specs for delete and backspace.

---
+++ spec/functions/basics.rb
@@ -0,0 +1,29 @@
+require 'spec/preparation'
+
+describe 'A Diakonos user can' do
+
+  before do
+    @d = $diakonos
+    @b = @d.open_file( SAMPLE_FILE )
+    cursor_should_be_at 0,0
+  end
+
+  after do
+    @d.close_file @b, Diakonos::CHOICE_NO_TO_ALL
+  end
+
+  it 'delete the current character' do
+    @d.delete
+    cursor_should_be_at 0,0
+    @b.to_a[ 0 ].should.equal '!/usr/bin/env ruby'
+  end
+
+  it 'backspace the previous character' do
+    3.times{ @d.cursor_right }
+    cursor_should_be_at 0,3
+    @d.backspace
+    @b.to_a[ 0 ].should.equal '#!usr/bin/env ruby'
+    cursor_should_be_at 0,2
+  end
+
+end
