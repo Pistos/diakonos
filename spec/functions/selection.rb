231c83b2a8bf7cb453e7d79d763b2c6e46cb3e59
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-04-20 22:06:03 -0400

    Spec for Buffer#remove_selection.

--- spec/functions/selection.rb
+++ spec/functions/selection.rb
@@ -5,10 +5,10 @@ describe 'Diakonos' do
   before do
     @d = $diakonos
     @b = @d.openFile( SAMPLE_FILE )
+    cursor_should_be_at 0,0
   end
 
-  it 'can select text' do
-    cursor_should_be_at 0,0
+  it 'lets you select text' do
     @d.anchor_selection
     @d.cursor_down
     @d.cursor_down
@@ -20,6 +20,16 @@ describe 'Diakonos' do
     selection.start_col.should.equal 0
     selection.end_row.should.equal 3
     selection.end_col.should.equal 0
+  end
+
+  it 'lets you stop selecting text' do
+    @b.selection_mark.should.be.nil
+    @d.anchor_selection
+    @d.cursor_down
+    @d.cursor_down
+    @b.selection_mark.should.not.be.nil
+    @d.remove_selection
+    @b.selection_mark.should.be.nil
   end
 
 end
