3514f934164fd2705d9d8bdc6591aa3bdfd6cd59
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-04-29 19:28:01 -0400

    Moved tag-related functions into functions/tags.rb.

--- lib/diakonos/functions.rb
+++ lib/diakonos/functions.rb
@@ -60,49 +60,6 @@ module Diakonos
       end
     end
 
-    def go_to_tag( tag_ = nil )
-      load_tags
-
-      # If necessary, prompt for tag name.
-
-      if tag_.nil?
-        if @current_buffer.changing_selection
-          selected_text = @current_buffer.copy_selection[ 0 ]
-        end
-        tag_name = get_user_input( "Tag name: ", @rlh_general, ( selected_text or "" ), @tags.keys )
-      else
-        tag_name = tag_
-      end
-
-      tag_array = @tags[ tag_name ]
-      if tag_array and tag_array.length > 0
-        if i = tag_array.index( @last_tag )
-          tag = ( tag_array[ i + 1 ] or tag_array[ 0 ] )
-        else
-          tag = tag_array[ 0 ]
-        end
-        @last_tag = tag
-        @tag_stack.push [ @current_buffer.name, @current_buffer.last_row, @current_buffer.last_col ]
-        if switch_to( @buffers[ tag.file ] )
-          #@current_buffer.go_to_line( 0 )
-        else
-          open_file tag.file
-        end
-        line_number = tag.command.to_i
-        if line_number > 0
-          @current_buffer.go_to_line( line_number - 1 )
-        else
-          find( "down", CASE_SENSITIVE, tag.command )
-        end
-      elsif tag_name
-        set_iline "No such tag: '#{tag_name}'"
-      end
-    end
-
-    def go_to_tag_under_cursor
-      go_to_tag @current_buffer.word_under_cursor
-    end
-
     def help( prefill = '' )
       if ! File.exist?( @help_dir ) || Dir[ "#{@help_dir}/*" ].size == 0
         set_iline 'There are no help files installed.'
@@ -223,18 +180,6 @@ module Diakonos
           @playing_macro = false
           @macro_input_history = nil
         end
-      end
-    end
-
-    def pop_tag
-      tag = @tag_stack.pop
-      if tag
-        if not switch_to( @buffers[ tag[ 0 ] ] )
-          open_file tag[ 0 ]
-        end
-        @current_buffer.cursor_to( tag[ 1 ], tag[ 2 ], Buffer::DO_DISPLAY )
-      else
-        set_iline "Tag stack empty."
       end
     end
 
--- lib/diakonos/version.rb
+++ lib/diakonos/version.rb
@@ -1,4 +1,4 @@
 module Diakonos
   VERSION       = '0.8.9'
-  LAST_MODIFIED = 'April 28, 2009'
+  LAST_MODIFIED = 'April 29, 2009'
 end
