1e5c04c5580f15f84fb05094db4e6021260473c1
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-14 10:45:03 -0400

    Changed spec/searching.rb to use path relative to spec file/dir instead of hard-coded absolute path.

--- spec/searching.rb
+++ spec/searching.rb
@@ -13,12 +13,14 @@ describe 'Diakonos' do
   end
 
   it 'greps the open buffers' do
+    dir = File.dirname( File.expand_path( __FILE__ ) )
+
     @d.actually_grep( 'inspect', @d.current_buffer )
     lines = File.readlines( @d.list_filename )
-    lines[ 0 ].should.match %r(^sample-file\.rb:13:   def inspection {100,} \| /misc/git/diakonos/spec/test-files/sample-file\.rb:13\n$)
-    lines[ 1 ].should.match %r(^sample-file\.rb:14:     x\.inspect {100,} \| /misc/git/diakonos/spec/test-files/sample-file\.rb:14\n$)
-    lines[ 2 ].should.match %r(^sample-file\.rb:15:     y\.inspect {100,} \| /misc/git/diakonos/spec/test-files/sample-file\.rb:15\n$)
-    lines[ 3 ].should.match %r(^sample-file\.rb:20: s\.inspection {100,} \| /misc/git/diakonos/spec/test-files/sample-file\.rb:20\n$)
+    lines[ 0 ].should.match %r(^sample-file\.rb:13:   def inspection {100,} \| #{dir}/test-files/sample-file\.rb:13\n$)
+    lines[ 1 ].should.match %r(^sample-file\.rb:14:     x\.inspect {100,} \| #{dir}/test-files/sample-file\.rb:14\n$)
+    lines[ 2 ].should.match %r(^sample-file\.rb:15:     y\.inspect {100,} \| #{dir}/test-files/sample-file\.rb:15\n$)
+    lines[ 3 ].should.match %r(^sample-file\.rb:20: s\.inspection {100,} \| #{dir}/test-files/sample-file\.rb:20\n$)
     lines.size.should.equal 4
     @d.close_list_buffer
   end
