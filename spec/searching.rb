d92f4db5a6f78cc74c08191619d6cbddfdf50b08
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-06-14 00:06:42 -0400

    Added specs for Buffer::pos_of_next.

--- spec/searching.rb
+++ spec/searching.rb
@@ -1,6 +1,6 @@
 require 'spec/preparation'
 
-describe 'Diakonos' do
+describe 'A Diakonos user' do
 
   before do
     @d = $diakonos
@@ -12,7 +12,7 @@ describe 'Diakonos' do
     @d.close_file @b, Diakonos::CHOICE_NO_TO_ALL
   end
 
-  it 'greps the open buffers' do
+  it 'can grep the open buffers' do
     dir = File.dirname( File.expand_path( __FILE__ ) )
 
     @d.actually_grep( 'inspect', @d.current_buffer )
@@ -23,6 +23,30 @@ describe 'Diakonos' do
     lines[ 3 ].should.match %r(^sample-file\.rb:20: s\.inspection {100,} \| #{dir}/test-files/sample-file\.rb:20\n$)
     lines.size.should.equal 4
     @d.close_list_buffer
+  end
+
+end
+
+describe 'A Diakonos Buffer' do
+
+  before do
+    @d = $diakonos
+    @b = @d.open_file( BRACKET_FILE )
+    cursor_should_be_at 0,0
+  end
+
+  after do
+    @d.close_file @b, Diakonos::CHOICE_NO_TO_ALL
+  end
+
+  it 'can find next closest characters' do
+    @b.pos_of_next( /x/, 0, 0 ).should.equal [ 1, 2, 'x' ]
+    @b.pos_of_next( /a/, 0, 0 ).should.equal [ 2, 4, 'a' ]
+    @b.pos_of_next( /b/, 0, 0 ).should.equal [ 3, 4, 'b' ]
+    @b.pos_of_next( /\}/, 0, 0 ).should.equal [ 5, 15, '}' ]
+
+    @b.pos_of_next( /:/, 1, 2 ).should.equal [ 1, 3, ':' ]
+    @b.pos_of_next( /a/, 2, 4 ).should.equal [ 2, 4, 'a' ]
   end
 
 end
