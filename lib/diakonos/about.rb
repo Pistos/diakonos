6356e9b8fba883bbd0809b39ab9d53771eee32ad
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-06-27 12:25:31 -0400

    Added more info to about page.

--- lib/diakonos/about.rb
+++ lib/diakonos/about.rb
@@ -3,12 +3,31 @@ module Diakonos
 
     def about_write
       File.open( @about_filename, "w" ) do |f|
+        inst = ::Diakonos::INSTALL_SETTINGS
         f.puts %{
 # About Diakonos
 
-## Version Information
+## Version
 
-Version:      #{::Diakonos::VERSION}
+Version:        #{ ::Diakonos::VERSION }
+Code Date:      #{ ::Diakonos::LAST_MODIFIED }
+Install Time:   #{ File.mtime( File.join( inst[ :lib_dir ], 'diakonos', 'installation.rb' ) ) }
+
+## Extensions
+
+
+
+## Paths
+
+Home dir:       #{ @diakonos_home }
+
+### Installation
+
+Prefix:             #{ inst[ :prefix ] }
+Executable dir:     #{ inst[ :bin_dir ] }
+Help dir:           #{ inst[ :help_dir ] }
+System config dir:  #{ inst[ :conf_dir ] }
+System library dir: #{ inst[ :lib_dir ] }
         }.strip
       end
     end
