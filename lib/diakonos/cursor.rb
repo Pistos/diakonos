d4fd2c82d87d459bb261099ae665f570946f2c3e
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-26 15:44:00 -0400

    Moved push_cursor_state out of Diakonos::Functions, and into cursor.rb.

--- lib/diakonos/cursor.rb
+++ lib/diakonos/cursor.rb
@@ -6,4 +6,21 @@ module Diakonos
       }
     end
   end
+
+  def push_cursor_state( top_line, row, col, clear_stack_pointer = CLEAR_STACK_POINTER )
+    new_state = {
+      buffer: @current_buffer,
+      top_line: top_line,
+      row: row,
+      col: col
+    }
+    if ! @cursor_stack.include? new_state
+      @cursor_stack << new_state
+      if clear_stack_pointer
+        @cursor_stack_pointer = nil
+      end
+      clear_non_movement_flag
+    end
+  end
+
 end
