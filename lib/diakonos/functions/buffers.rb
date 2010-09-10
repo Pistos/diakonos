f4ef7f88108a7f0958428fb8edb33388860de24d
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-09-10 14:14:50 -0400

    A little refactoring of close_file.  (4e53fe2)
    Conflicts:
    
    	lib/diakonos/functions/buffers.rb

--- lib/diakonos/functions/buffers.rb
+++ lib/diakonos/functions/buffers.rb
@@ -11,93 +11,92 @@ module Diakonos
     # @see Diakonos::CHOICE_YES
     # @see Diakonos::CHOICE_NO
     def close_file( buffer = buffer_current, to_all = nil )
-      return nil if buffer.nil?
+      return nil  if buffer.nil?
 
       choice = nil
-      if @buffers.has_value?( buffer )
-        do_closure = true
-
-        if buffer.modified?
-          if not buffer.read_only
-            if to_all.nil?
-              choices = [ CHOICE_YES, CHOICE_NO, CHOICE_CANCEL ]
-              if @quitting
-                choices.concat [ CHOICE_YES_TO_ALL, CHOICE_NO_TO_ALL ]
-              end
-              choice = get_choice(
-                "Save changes to #{buffer.nice_name}?",
-                choices,
-                CHOICE_CANCEL
-              )
-            else
-              choice = to_all
-            end
-
-            case choice
-            when CHOICE_YES, CHOICE_YES_TO_ALL
-              do_closure = true
-              save_file buffer
-            when CHOICE_NO, CHOICE_NO_TO_ALL
-              do_closure = true
-            when CHOICE_CANCEL
-              do_closure = false
-            end
-          end
-        end
-
-        if do_closure
-          del_buffer_key = nil
-          del_buffer = nil
-          previous_buffer = nil
-          to_switch_to = nil
-          switching = false
-
-          # Search the buffer hash for the buffer we want to delete,
-          # and mark the one we will switch to after deletion.
-          @buffers.each do |buffer_key,buf|
-            if switching
-              to_switch_to = buf
-              break
-            end
-            if buf == buffer
-              del_buffer_key = buffer_key
-              del_buffer = buf
-              switching = true
-              next
-            end
-            previous_buffer = buf
-          end
-
-          buf = nil
-          while(
-            ( not @buffer_stack.empty? ) and
-            ( not @buffers.values.include?( buf ) ) or
-            ( @buffers.key( buf ) == del_buffer_key )
-          ) do
-            buf = @buffer_stack.pop
-          end
-          if @buffers.values.include?( buf )
-            to_switch_to = buf
-          end
-
-          if to_switch_to
-            switch_to( to_switch_to )
-          elsif previous_buffer
-            switch_to( previous_buffer )
-          else
-            # No buffers left.  Open a new blank one.
-            open_file
-          end
-
-          @buffers.delete del_buffer_key
-          cursor_stack_remove_buffer del_buffer
-          save_session
-
-          update_status_line
-          update_context_line
-        end
-      else
+      if ! @buffers.has_value?( buffer )
         log "No such buffer: #{buffer.name}"
+        return nil
+      end
+
+      do_closure = true
+
+      if buffer.modified? && ! buffer.read_only
+        if to_all
+          choice = to_all
+        else
+          choices = [ CHOICE_YES, CHOICE_NO, CHOICE_CANCEL ]
+          if @quitting
+            choices.concat [ CHOICE_YES_TO_ALL, CHOICE_NO_TO_ALL ]
+          end
+          choice = get_choice(
+            "Save changes to #{buffer.nice_name}?",
+            choices,
+            CHOICE_CANCEL
+          )
+        end
+
+        case choice
+        when CHOICE_YES, CHOICE_YES_TO_ALL
+          do_closure = true
+          save_file buffer
+        when CHOICE_NO, CHOICE_NO_TO_ALL
+          do_closure = true
+        when CHOICE_CANCEL
+          do_closure = false
+        end
+      end
+
+      if do_closure
+        del_buffer_key = nil
+        del_buffer = nil
+        previous_buffer = nil
+        to_switch_to = nil
+        switching = false
+
+        # Search the buffer hash for the buffer we want to delete,
+        # and mark the one we will switch to after deletion.
+        @buffers.each do |buffer_key,buf|
+          if switching
+            to_switch_to = buf
+            break
+          end
+          if buf == buffer
+            del_buffer_key = buffer_key
+            del_buffer = buf
+            switching = true
+            next
+          end
+          previous_buffer = buf
+        end
+
+        buf = nil
+        while(
+          ( not @buffer_stack.empty? ) and
+          ( not @buffers.values.include?( buf ) ) or
+          ( @buffers.key( buf ) == del_buffer_key )
+        ) do
+          buf = @buffer_stack.pop
+        end
+        if @buffers.values.include?( buf )
+          to_switch_to = buf
+        end
+
+        if to_switch_to
+          switch_to( to_switch_to )
+        elsif previous_buffer
+          switch_to( previous_buffer )
+        else
+          # No buffers left.  Open a new blank one.
+          open_file
+        end
+
+        @buffers.delete del_buffer_key
+        cursor_stack_remove_buffer del_buffer
+        save_session
+
+        update_status_line
+        update_context_line
       end
 
       choice
