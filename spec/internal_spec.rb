3debef855de49a6d97af1b9a0828846af611b849
Pistos <git dawt pistos aet purepistos dawt net>
2015-04-18 12:41:23 -0400

    Convert internal spec from bacon to rspec.

--- spec/internal_spec.rb
+++ spec/internal_spec.rb
@@ -1,15 +1,15 @@
-require_relative 'preparation'
+require 'spec_helper'
 
-describe '::Diakonos can' do
+RSpec.describe '::Diakonos can' do
 
   it 'parse a filename and line number from a String' do
-    ::Diakonos.parse_filename_and_line_number( 'abc.rb' ).should.equal [ 'abc.rb', nil ]
-    ::Diakonos.parse_filename_and_line_number( '/absolute/path/abc.rb' ).should.equal [ '/absolute/path/abc.rb', nil ]
-    ::Diakonos.parse_filename_and_line_number( 'abc.rb:1' ).should.equal [ 'abc.rb', 0 ]
-    ::Diakonos.parse_filename_and_line_number( 'abc.rb:5' ).should.equal [ 'abc.rb', 4 ]
-    ::Diakonos.parse_filename_and_line_number( 'abc.rb:10' ).should.equal [ 'abc.rb', 9 ]
-    ::Diakonos.parse_filename_and_line_number( '/absolute/path/abc.rb:15' ).should.equal [ '/absolute/path/abc.rb', 14 ]
-    ::Diakonos.parse_filename_and_line_number( 'relative/path/abc.rb:15' ).should.equal [ 'relative/path/abc.rb', 14 ]
+    expect(::Diakonos.parse_filename_and_line_number('abc.rb')).to eq ['abc.rb', nil]
+    expect(::Diakonos.parse_filename_and_line_number('/absolute/path/abc.rb')).to eq ['/absolute/path/abc.rb', nil]
+    expect(::Diakonos.parse_filename_and_line_number('abc.rb:1')).to eq ['abc.rb', 0]
+    expect(::Diakonos.parse_filename_and_line_number('abc.rb:5')).to eq ['abc.rb', 4]
+    expect(::Diakonos.parse_filename_and_line_number('abc.rb:10')).to eq ['abc.rb', 9]
+    expect(::Diakonos.parse_filename_and_line_number('/absolute/path/abc.rb:15')).to eq ['/absolute/path/abc.rb', 14]
+    expect(::Diakonos.parse_filename_and_line_number('relative/path/abc.rb:15')).to eq ['relative/path/abc.rb', 14]
   end
 
 end
