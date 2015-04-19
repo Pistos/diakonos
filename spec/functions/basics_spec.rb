c34bca0715b8f305d7fe9731d67bfc6d330f35eb
Pistos <git dawt pistos aet purepistos dawt net>
2015-04-19 01:58:49 -0400

    Convert Buffer basics spec from bacon to rspec.

--- spec/functions/basics_spec.rb
+++ spec/functions/basics_spec.rb
@@ -1,6 +1,6 @@
-require_relative '../preparation'
+require 'spec_helper'
 
-describe 'A Diakonos user can' do
+RSpec.describe 'A Diakonos user can' do
 
   before do
     @d = $diakonos
@@ -15,14 +15,14 @@ describe 'A Diakonos user can' do
   it 'delete the current character' do
     @d.delete
     cursor_should_be_at 0,0
-    @b.to_a[ 0 ].should.equal '!/usr/bin/env ruby'
+    expect(@b.to_a[ 0 ]).to eq '!/usr/bin/env ruby'
   end
 
   it 'backspace the previous character' do
     3.times{ @d.cursor_right }
     cursor_should_be_at 0,3
     @d.backspace
-    @b.to_a[ 0 ].should.equal '#!usr/bin/env ruby'
+    expect(@b.to_a[ 0 ]).to eq '#!usr/bin/env ruby'
     cursor_should_be_at 0,2
   end
 
@@ -31,7 +31,7 @@ describe 'A Diakonos user can' do
     @d.carriage_return
     cursor_should_be_at 1,0
     lines = @b.to_a
-    lines[ 0..3 ].should.equal [
+    expect(lines[ 0..3 ]).to eq [
       '#!/us',
       'r/bin/env ruby',
       '',
