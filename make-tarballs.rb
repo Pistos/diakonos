677c7b6d8e50ac820987e59bac586965ff5b6fa6
Pistos <git dawt pistos aet purepistos dawt net>
2024-11-11 23:06:43 -0500

    Remove old release-making scripts

--- make-tarballs.rb
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script is only for quickly making the tarballs.
-# The make-release.rb script is the full script to run for making a release.
-
-VERSION=`grep VERSION lib/diakonos/version.rb | head -n 1 | egrep -o '[0-9.]+'`
-git archive --format=tar --prefix=diakonos-${VERSION}/ HEAD | bzip2 > diakonos-${VERSION}.tar.bz2
-git archive --format=tar --prefix=diakonos-${VERSION}/ HEAD | gzip > diakonos-${VERSION}.tar.gz
